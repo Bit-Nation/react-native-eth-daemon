b456ecf45e2308c688200a4e1c108e29414f6acf
Alberto Estarrona <alberto@estarrona.me>
2017-12-20 11:52:34 -0600

    Testing-ios
    Initial repo with iOS experiments

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ import RNEthDaemon from 'react-native-eth-daemon';
 // TODO: What to do with the module?
 RNEthDaemon;
 ```
-  
+
 ## Development
 - We are using [this](http://nvie.com/posts/a-successful-git-branching-model/) git workflow. Please make sure to read it.
 - Make sure to prefix your git commit's with an "topic" like to `[git] blacklisted .idea folder`
--- ios/RNEthDaemon.m
+++ ios/RNEthDaemon.m
@@ -1,5 +1,6 @@
 
 #import "RNEthDaemon.h"
+#import <Geth/Geth.objc.h>
 
 @implementation RNEthDaemon
 
@@ -8,6 +9,44 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(startDaemon) {
+    GethNodeConfig *configGeth;
+    NSError *error = nil;
+    
+    configGeth = GethNewNodeConfig();
+    [configGeth setEthereumEnabled:true];
+    [configGeth setEthereumNetworkID:3];
+    
+    GethEnodes *enodes = GethNewEnodes(16);
+    enodes = GethFoundationBootnodes();
+    
+    [configGeth setBootstrapNodes:enodes];
+    NSString *genesis = GethTestnetGenesis();
+    [configGeth setEthereumGenesis: genesis];
+    [configGeth  setMaxPeers: 25];
+    [configGeth setWhisperEnabled: NO];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSURL *rootUrl =[[fileManager
+                      URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask]
+                     lastObject];
+    NSURL *testnetFolderName = [rootUrl URLByAppendingPathComponent:@"ethereum/testnet"];
+    
+    if (![fileManager fileExistsAtPath:testnetFolderName.path])
+        [fileManager createDirectoryAtPath:testnetFolderName.path withIntermediateDirectories:YES attributes:nil error:&error];
+    
+    NSString *networkDir = [rootUrl.path stringByAppendingString:@"/$TMPDIR"];
+    node = GethNewNode(networkDir, configGeth, &error);
+    GethSetVerbosity(9);
+    
+    [node start:&error];
+}
+
+RCT_EXPORT_METHOD(stopDaemon){
+    NSError *error = nil;
+    [node stop:&error];
+}
 
 @end
   
