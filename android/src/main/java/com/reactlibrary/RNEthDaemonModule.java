93581399bfea68c16c3b7d0fdf342aadadfce949
Alberto Estarrona <alberto@estarrona.me>
2017-12-26 15:07:29 -0600

    [android] classes added

--- android/src/main/java/com/reactlibrary/RNEthDaemonModule.java
+++ android/src/main/java/com/reactlibrary/RNEthDaemonModule.java
@@ -4,9 +4,17 @@ package com.reactlibrary;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import org.ethereum.geth.Geth;
+import org.ethereum.geth.Node;
+import org.ethereum.geth.NodeConfig;
+
+import java.io.File;
 
 public class RNEthDaemonModule extends ReactContextBaseJavaModule {
+
+  private Node node;
+  final String TAG = "GethNode";
 
   private final ReactApplicationContext reactContext;
 
@@ -18,5 +26,59 @@ public class RNEthDaemonModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNEthDaemon";
+  }
+
+  @ReactMethod
+  public void startDaemon() {
+    Log.d("GethNode", "Starting Geth process" );
+
+    NodeConfig nodeConfig = Geth.newNodeConfig();
+    Log.d("GethNode", "New node" );
+    nodeConfig.setEthereumDatabaseCache(16);
+    nodeConfig.setEthereumEnabled(true);
+    nodeConfig.setEthereumNetworkID(3);
+    String genesis = Geth.testnetGenesis();
+    nodeConfig.setEthereumGenesis(genesis);
+    nodeConfig.setMaxPeers(25);
+    nodeConfig.setWhisperEnabled(false);
+
+    Log.d("GethNode", "Configured" );
+
+    final String root = getReactApplicationContext().getBaseContext().getApplicationInfo().dataDir;
+    final String dataFolder = root + "/.ethereum";
+    Log.d("GethNode", "Starting Geth node in folder: " + dataFolder);
+
+    Log.d("GethNode", "Have the root/folder" );
+
+    try {
+      final File newFile = new File(dataFolder);
+      newFile.mkdir();
+    } catch (Exception e) {
+      Log.e(TAG, "error making folder: " + dataFolder, e);
+    }
+
+    final String networkDir = dataFolder + "/$TMPDIR";
+
+    node = new Node(networkDir, nodeConfig);
+    Log.d(TAG, "Node config " + nodeConfig);
+
+    try {
+      node.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    Log.d(TAG, "Geth node started");
+  }
+
+  @ReactMethod
+  public void stopDaemon() {
+    try {
+      node.stop();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    Log.d(TAG, "Geth node stoped");
   }
 }
