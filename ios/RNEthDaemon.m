561cb0b4aeb197a168979765b4a6692bb0750eb5
Alberto Estarrona <alberto@estarrona.me>
2017-12-28 14:25:02 -0600

    [ios] Added Promise responses
    startDaemon() and stopDaemon() functions responses are handled via Promise (async - await) in JS now

--- ios/RNEthDaemon.m
+++ ios/RNEthDaemon.m
@@ -11,10 +11,13 @@
 }
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(startDaemon:(NSDictionary *)config) {
+RCT_REMAP_METHOD(startDaemon,
+                 params:(NSDictionary *)config
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject) {
     GethNodeConfig *configGeth;
     NSError *error = nil;
-
+    
     configGeth = GethNewNodeConfig();
     [configGeth setEthereumEnabled: [RCTConvert BOOL:config[@"enabledEthereum"]]];
     long networkID = (long)[RCTConvert NSNumber:config[@"networkID"]];
@@ -29,30 +32,44 @@ RCT_EXPORT_METHOD(startDaemon:(NSDictionary *)config) {
     
     GethEnodes *enodes = GethNewEnodes((long)[RCTConvert NSNumber:config[@"enodesNumber"]]);
     enodes = GethFoundationBootnodes();
-
+    
     [configGeth setBootstrapNodes:enodes];
     [configGeth  setMaxPeers: (long)[RCTConvert NSNumber:config[@"maxPeers"]]];
     [configGeth setWhisperEnabled: [RCTConvert BOOL:config[@"enabledWhisper"]]];
-
+    
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSURL *rootUrl =[[fileManager
                       URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask]
                      lastObject];
     NSURL *testnetFolderName = [rootUrl URLByAppendingPathComponent:@"ethereum/testnet"];
-
+    
     if (![fileManager fileExistsAtPath:testnetFolderName.path])
         [fileManager createDirectoryAtPath:testnetFolderName.path withIntermediateDirectories:YES attributes:nil error:&error];
-
+    
     NSString *networkDir = [rootUrl.path stringByAppendingString:@"/$TMPDIR"];
     node = GethNewNode(networkDir, configGeth, &error);
     GethSetVerbosity(9);
-
+    
     [node start:&error];
+    if (error == nil) {
+        resolve(@"Node successfully started");
+    } else {
+        reject(@"node_error", @"Could not start the node", error);
+    }
 }
 
-RCT_EXPORT_METHOD(stopDaemon){
+RCT_REMAP_METHOD(stopDaemon,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject) {
+    
     NSError *error = nil;
     [node stop:&error];
+    
+    if (error == nil) {
+        resolve(@"Node successfully stopped");
+    } else {
+        reject(@"node_error", @"Could not stop the node", error);
+    }
 }
 
 @end
